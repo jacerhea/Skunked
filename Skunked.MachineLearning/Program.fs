01a2cd8c75fa3c278de612053d236243934fb41b
Jace <jace.rhea@gmail.com>
2018-11-29 21:53:51 -0600

    Missed this.

--- Skunked.MachineLearning/Program.fs
+++ Skunked.MachineLearning/Program.fs
@@ -3,22 +3,25 @@
 open System
 open Skunked
 open System.Collections.Generic
+open Skunked.AI.Play
 open Skunked.Rules
 open Microsoft.FSharp.Collections
 open Skunked.PlayingCards
 open Combinatorics.Collections;
+open System.Collections.ObjectModel
 
 
-let GenerateHands = 
-    let deck = new Deck()
-    let combinations = new Combinations<Card>(new System.Collections.Generic.List<Card>(deck), 4)
-    seq{for combo in combinations -> combo}
+//let GenerateHands = 
+//    let deck = new Deck()
+//    let combinations = new Combinations<Card>(new System.Collections.Generic.List<Card>(deck), 4)
+//    seq{for combo in combinations -> combo}
 
 
-let ToKey(turn:Boolean, hand: list<Card>, pile: list<Card>)  =
-    let key = (if turn then 0 else 1).ToString()
-    let x = hand |> List.fold (fun acc elem -> acc + elem.Rank.ToString()) ""
-    let y= key + x
+//let ToKey(turn:Boolean, hand: seq<Card>, pile: list<Card>)  =
+//    let key = (if turn then 0 else 1).ToString()
+//    let x = hand |> List.fold (fun acc elem -> acc + elem.Rank.ToString()) ""
+//    let y= key + x
+//    y
 
 let AreEqual(source1: Set<_>, source2: Set<_>) = 
             if source1.Count <> source2.Count then
@@ -29,8 +32,12 @@ let AreEqual(source1: Set<_>, source2: Set<_>) =
 
 [<EntryPoint>]
 let main argv =
-    let hands = GenerateHands |> Seq.toList
+    let strategy = new MaxDifferencePlayStrategy();
 
 
-
+    //let hands = GenerateHands 
+    //            |> Seq.toList 
+    //            |> Seq.map(fun hand -> (hand, strategy.GetRemainingPossibilities(new Skunked.AI.Play.MaxDifferencePlayStrategy.PlayState())))
+    //            |> Seq.map(fun hand -> (ToKey(true, hand |> Seq.toList, []), hand) ) 
+    //            |> Seq.toList
     3
