984aa04f7b0deceac13037c6773bfee0b8504283
Jace <jace.rhea@gmail.com>
2021-01-17 23:43:45 -0600

    Web app.

---
+++ src/Skunked.Web/Controllers/DeckController.fs
@@ -0,0 +1,19 @@
+﻿namespace Skunked.Web.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open Skunked.Web
+open Skunked.Cards
+
+[<ApiController>]
+[<Route("[controller]")>]
+type DeckController (logger : ILogger<DeckController>) =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member _.Get() =
+        new Deck()
