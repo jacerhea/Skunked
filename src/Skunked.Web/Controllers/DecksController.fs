dc82d9040cf1b9153b01363ca72f77d767408972
Jace <jace.rhea@gmail.com>
2025-07-27 10:01:49 -0500

    Restarting the web projects with latest F# templates.

--- src/Skunked.Web/Controllers/DecksController.fs
+++ src/Skunked.Web/Controllers/DecksController.fs
@@ -6,14 +6,12 @@ open System.Linq
 open System.Threading.Tasks
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Logging
-open Skunked.Web
 open Skunked.Cards
 
 [<ApiController>]
 [<Route("[controller]")>]
-type DeckController (logger : ILogger<DeckController>) =
+type DecksController() =
     inherit ControllerBase()
 
     [<HttpGet>]
-    member _.Get() =
-        new Deck()
+    member _.Get() = Deck()
--- src/Skunked.Web/Program.fs
+++ src/Skunked.Web/Program.fs
@@ -1,27 +1,36 @@
 namespace Skunked.Web
-
+#nowarn "20"
 open System
 open System.Collections.Generic
 open System.IO
 open System.Linq
 open System.Threading.Tasks
 open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.HttpsPolicy
 open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
 
 module Program =
     let exitCode = 0
 
-    let CreateHostBuilder args =
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(fun webBuilder ->
-                webBuilder.UseStartup<Startup>() |> ignore
-            )
-
     [<EntryPoint>]
     let main args =
-        CreateHostBuilder(args).Build().Run()
+
+        let builder = WebApplication.CreateBuilder(args)
+
+        builder.Services.AddControllers()
+
+        let app = builder.Build()
+
+        app.UseHttpsRedirection()
+
+        app.UseAuthorization()
+        app.MapControllers()
+
+        app.Run()
 
         exitCode
